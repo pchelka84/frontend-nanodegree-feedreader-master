40f94c7a01d4958f9d7d49193c592dde714b1f3a
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-07-12 14:50:07 -0400

    Each feed has name defined and it's not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,11 +26,10 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
-         */  
+         */
         it('each feed has a URL defined and it is not empty', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.url).toBeDefined();
@@ -38,10 +37,18 @@ $(function() {
             })
         });
 
+
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
-         */  
+         */
+        it('each feed has a name defined and it is not empty', function () {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            })
+        });
+    }); 
 
     /* TODO: Write a new test suite named "The menu" */
 
